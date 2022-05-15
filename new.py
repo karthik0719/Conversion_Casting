2c98058fce8603188fa30d38ca91c9023b56487c
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-15 21:17:33 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,16 @@
+'''My Name is Karthik. Qualification is B.tech
+   Age is 23 and percentage is 71.22%''' 
+
+Name='Karthik'
+Qulification='B tech'
+Age=23
+Percentage=71.22 
+
+# print('My Name is',Name+".","Qualification is",Qulification)
+# print('Age is',Age,"percentage is",str(Percentage)+'%')
+
+# print('My Name is'+" "+Name+"."+" "+"Qualification is",Qulification)
+# print('Age is'+" "+str(Age),"percentage is"+" "+str(Percentage)+'%')
+
+print('My Name is %s.Qualification is %s'%(Name,Qulification))
+print('Age is %d and percentage is %.2f'%(Age,Percentage))
